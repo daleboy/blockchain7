16f1666a0434ba02e81b3a8acb3463ca0dbf6bca
daleboy <785909868@qq.com>
2022-04-09 15:45:48 +0800

    update to go 1.18

--- merkle_tree_test.go
+++ merkle_tree_test.go
@@ -3,12 +3,11 @@ package blockchain7
 import (
 	"encoding/hex"
 	"fmt"
-	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewMerkleNode(t *testing.T) {
+func TestNewMerkleNode(t *assert.TestingT) {
 	data := [][]byte{
 		[]byte("node1"),
 		[]byte("node2"),
@@ -30,26 +29,26 @@ func TestNewMerkleNode(t *testing.T) {
 	n7 := NewMerkleNode(n5, n6, nil)
 
 	assert.Equal(
-		t,
+		*t,
 		"64b04b718d8b7c5b6fd17f7ec221945c034cfce3be4118da33244966150c4bd4",
 		hex.EncodeToString(n5.Data),
 		"Level 1 hash 1 is correct",
 	)
 	assert.Equal(
-		t,
+		*t,
 		"08bd0d1426f87a78bfc2f0b13eccdf6f5b58dac6b37a7b9441c1a2fab415d76c",
 		hex.EncodeToString(n6.Data),
 		"Level 1 hash 2 is correct",
 	)
 	assert.Equal(
-		t,
+		*t,
 		"4e3e44e55926330ab6c31892f980f8bfd1a6e910ff1ebc3f778211377f35227e",
 		hex.EncodeToString(n7.Data),
 		"Root hash is correct",
 	)
 }
 
-func TestNewMerkleTree(t *testing.T) {
+func TestNewMerkleTree(t *assert.TestingT) {
 	data := [][]byte{
 		[]byte("node1"),
 		[]byte("node2"),
@@ -71,5 +70,5 @@ func TestNewMerkleTree(t *testing.T) {
 	rootHash := fmt.Sprintf("%x", n7.Data)
 	mTree := NewMerkleTree(data)
 
-	assert.Equal(t, rootHash, fmt.Sprintf("%x", mTree.RootNode.Data), "Merkle tree root hash is correct")
+	assert.Equal(*t, rootHash, fmt.Sprintf("%x", mTree.RootNode.Data), "Merkle tree root hash is correct")
 }
